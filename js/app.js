e1de4710ac4f17c20a502a8e9d710d72a721385f
channy <ox043440@gmail.com>
2023-10-13 18:27:06 +0900

    231013

--- js/app.js
+++ js/app.js
@@ -3,6 +3,26 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.stroke();
+ctx.lineWidth = 2;
+
+let isPainting = false;
+function onMove(event){
+    if(isPainting){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+};
+
+function onMouseDown(){
+    isPainting = true;
+};
+
+function onMouseUp(){
+    isPainting = false;
+};
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", onMouseUp);
