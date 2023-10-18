025ba3220741a95c62d105f1d25ac93915dc35b9
channy <ox043440@gmail.com>
2023-10-18 18:29:11 +0900

    231018

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
+const textInput = document.getElementById("text");
+const fileInput = document.getElementById("file")
+const eraserBtn = document.getElementById("eraser-btn") 
 const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn")
 const colorOptions = Array.from(
     document.getElementsByClassName("color-option")
 );
@@ -6,10 +10,15 @@ const color = document.getElementById("color")
 const lineWidth = document.getElementById("line-width")
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 
 let isPainting = false;
 let isFilling = false;
@@ -60,10 +69,44 @@ function onModeClick(event){
 
 function onCanvasClick(){
     if(isFilling){
-        ctx.fillRect(0, 0, 800, 800);
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+};
+
+function onDestroyClick(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+};
+
+function oneraserClick(){
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill"
+};
+
+function onFileChange(event){
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src = url;
+    image.onload = function(){
+        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        fileInput.value = null;
+    }
+};
+
+function onDoubleClick(event){
+    const text = textInput.value;
+    if(text !== ""){
+        ctx.save();
+        ctx.lineWidth = 1;
+        ctx.font = "68px serif"
+        ctx.fillText(text, event.offsetX, event.offsetY);
+        ctx.restore();   
     }
 }
 
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -74,5 +117,7 @@ color.addEventListener("change", onColorChange);
 
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
 
-modeBtn.addEventListener("click", onModeClick)
-
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", oneraserClick);
+fileInput.addEventListener("change", onFileChange)
